77f6f42d415b7903715398e0c6a12cd67f889430
Malikkhanking <usmanmalikkhan233@gmail.com>
2024-11-20 23:17:24 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ const databasePath = path.join(__dirname, './database.db')
 if (existsSync(configPath)) require('dotenv').config({ path: configPath })
 const toBool = (x) => x == 'true'
 const DATABASE_URL =
-process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
+process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL 
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'MALIKKHAN99').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
@@ -28,7 +28,7 @@ module.exports = {
           logging: false,
         }),
   PREFIX: (process.env.PREFIX || '^[.,!]').trim(),
-  SUDO: process.env.SUDO || '',
+  SUDO: process.env.SUDO || '+923436062975',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
